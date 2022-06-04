c56bca4b994e7a48bf9c0a005f0dadbf38dc6a60
ryu <kakaomame1942@gmail.com>
2022-06-05 00:23:29 +0900

    add struct

--- lesson/main.go
+++ lesson/main.go
@@ -1,23 +1,7 @@
 package main
 
-import "fmt"
-
 type Vertex struct {
-	X, Y int
+	x, y int
 }
 
-// Vertexのstructと紐づけている
-func (v Vertex) Area() int {
-	return v.X * v.Y
-}
-
-// func Area(v Vertex) int {
-// 	return v.X * v.Y
-// }
-
-func main() {
-	v := Vertex{3, 4}
-	// fmt.Println(Area(v))
-	// vの呼び出し関数として利用可能
-	fmt.Println(v.Area())
-}
+// commit test
